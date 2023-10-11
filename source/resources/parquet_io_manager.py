3f59c02d18161387c221e25b98f387d0f849d0cc
Virginie Grosboillot <vestalisvirginis@gmail.com>
2023-10-11 12:05:16 +0000

    black and flake run

--- source/resources/parquet_io_manager.py
+++ source/resources/parquet_io_manager.py
@@ -1,11 +1,19 @@
 from dagster import ConfigurableIOManager
 
+from pyspark.sql import SparkSession
+
 
 class LocalParquetIOManager(ConfigurableIOManager):
     base_dir: str
 
     def _get_path(self, context):
-        return "/".join([context.resource_config["base_dir"], context.metadata["output_folder"], context.metadata["name"]])
+        return "/".join(
+            [
+                context.resource_config["base_dir"],
+                context.metadata["output_folder"],
+                context.metadata["name"],
+            ]
+        )
         # return "/".join([context.resource_config["base_dir"], context.op_config["output_folder"], context.op_config["name"]]) AttributeError: 'OutputContext' object has no attribute 'op_config'
 
     def handle_output(self, context, obj):
@@ -14,4 +22,3 @@ class LocalParquetIOManager(ConfigurableIOManager):
     def load_input(self, context):
         spark = SparkSession.builder.getOrCreate()
         return spark.read.parquet(self._get_path(context.upstream_output))
- 
