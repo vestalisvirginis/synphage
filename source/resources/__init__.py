3d95f8f084a99c9ab43daea2e73566c5ee0c6b86
Virginie Grosboillot <vestalisvirginis@gmail.com>
2023-10-15 12:30:24 +0000

    fix hard coded path for fs

--- source/resources/__init__.py
+++ source/resources/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from .ncbi_resource import ncbi_resource
 from .parquet_io_manager import LocalParquetIOManager
 
@@ -7,5 +9,5 @@ from dagster import EnvVar, FilesystemIOManager
 RESOURCES_LOCAL = {
     "ncbi_connection": ncbi_resource,
     "parquet_io_manager": LocalParquetIOManager(base_dir=EnvVar("PHAGY_DIRECTORY")),
-    "io_manager": FilesystemIOManager(base_dir=EnvVar("FILE_SYSTEM")),
+    "io_manager": FilesystemIOManager(base_dir="/".join([os.getenv("PHAGY_DIRECTORY"),os.getenv("FILE_SYSTEM")])),
 }
