0fc1471c3f4f80c0abddbcbfde2a040788b092fe
Virginie Grosboillot <vestalisvirginis@gmail.com>
2023-10-17 14:39:43 +0000

    fix file path

--- source/assets/status/status.py
+++ source/assets/status/status.py
@@ -3,7 +3,26 @@ from dagster import (
     op,
     graph_asset,
     Config,
+    Field,
+    EnvVar,
 )
+
+import os
+import pickle
+import json
+from pathlib import Path
+from datetime import datetime
+from typing import List
+from Bio import SeqIO
+
+
+def _standardise_file_extention(file) -> None:
+    """Change file extension when '.gbk' for '.gb'"""
+    path = Path(file)
+    if path.suffix == ".gbk":
+        return path.rename(path.with_suffix(".gb"))
+    else:
+        return path
 
 
 class FileConfig(Config):
@@ -22,7 +41,72 @@ def process_asset():
     return process_file()
 
 
-@asset()
-def downstream_asset(context, process_asset):
-    context.log.info("Downstream processing")
-    return "TEST"
+sqc_folder_config = {
+    "fs": Field(
+        str,
+        description="Path to folder containing the genbank files",
+        default_value="fs",
+    ),
+    "genbank_dir": Field(
+        str,
+        description="Path to folder containing the genbank files",
+        default_value="genbank",
+    ),
+    "fasta_dir": Field(
+        str,
+        description="Path to folder containing the fasta sequence files",
+        default_value="gene_identity/fasta",
+    ),
+}
+
+
+@asset(
+    config_schema={**sqc_folder_config},
+    description="""Update the list of sequences available in the genbank folder""",
+    compute_kind="Python",
+    io_manager_key="io_manager",
+    metadata={"owner": "Virginie Grosboillot"},
+)
+def list_genbank_files(context, process_asset) -> List[str]:
+    context.log.info(f"The following file {process_asset} is being processed")
+
+    # filepath = "/".join(
+    #     [os.getenv("PHAGY_DIRECTORY"), context.op_config["genbank_dir"], process_asset]
+    # )
+
+    # Standarise file extension
+    # new_path = _standardise_file_extention(filepath)
+    new_path = _standardise_file_extention(process_asset)
+    new_file = new_path.stem
+
+    # Load already processed files
+    path = "/".join(
+        [
+            os.getenv(EnvVar("PHAGY_DIRECTORY")),
+            context.op_config["fs"],
+            "list_genbank_files",
+        ]
+    )
+
+    if os.path.exists(path):
+        context.log.info("path exist")
+        files = pickle.load(open(path, "rb"))
+    else:
+        context.log.info("path do not exist")
+        files = []
+    context.log.info(files)
+
+    # Update file list
+    files.append(new_file)
+
+    time = datetime.now()
+    context.add_output_metadata(
+        metadata={
+            "text_metadata": f"Last update of the genbank list {time.isoformat()} (UTC).",
+            "processed_file": new_file,
+            "path": path,
+            "num_files": len(files),
+            "updated_list": files,
+        }
+    )
+    return files
