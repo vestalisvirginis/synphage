19e58d10b462a8a916e2a251c5f5804134332fe2
Virginie Grosboillot <vestalisvirginis@gmail.com>
2023-11-14 10:45:06 +0000

    fix test list genbank files

--- test/unit/status_asset/test_list_gb_files.py
+++ test/unit/status_asset/test_list_gb_files.py
@@ -9,7 +9,7 @@ def test_list_genbank_files(mock_env_phagy_dir_blasting):
     result = list_genbank_files(context)
     assert isinstance(result, tuple)
     assert isinstance(result[0], list)
-    assert len(result[0]) == 5
+    assert len(result[0]) == 6
     assert set(result[0]) == set(
         [
             PosixPath("test/fixtures/synthetic_data/genbank/TT_000001.gb"),
@@ -17,12 +17,13 @@ def test_list_genbank_files(mock_env_phagy_dir_blasting):
             PosixPath("test/fixtures/synthetic_data/genbank/TT_000003.gb"),
             PosixPath("test/fixtures/synthetic_data/genbank/TT_000004.gb"),
             PosixPath("test/fixtures/synthetic_data/genbank/TT_000005.gb"),
+            PosixPath("test/fixtures/synthetic_data/genbank/TT_000006.gb"),
         ]
     )
     assert isinstance(result[1], list)
-    assert len(result[1]) == 5
+    assert len(result[1]) == 6
     assert set(result[1]) == set(
-        ["TT_000001", "TT_000002", "TT_000003", "TT_000004", "TT_000005"]
+        ["TT_000001", "TT_000002", "TT_000003", "TT_000004", "TT_000005", "TT_000006"]
     )
 
 
@@ -34,8 +35,8 @@ def test_status_assets(mock_env_phagy_dir_blasting):
         "list_genbank_files", "standardised_ext_file"
     )
     genbank_files = result.output_for_node("list_genbank_files", "list_genbank_files")
-    assert len(standardised_files) == 5
-    assert len(genbank_files) == 5
+    assert len(standardised_files) == 6
+    assert len(genbank_files) == 6
 
 
 # test on output metadat?
