377b6bfbb35026ed44aede19e11c3a3bc4b5f751
Virginie Grosboillot <vestalisvirginis@gmail.com>
2023-11-16 07:51:08 +0000

    test case for sqc id

---
+++ test/unit/viewer_asset/test_get_sqc_identity.py
@@ -0,0 +1,10 @@
+from synphage.assets.viewer.viewer import _get_sqc_identity_from_csv
+
+def test_get_sqc_identity_from_csv():
+    path = 'test/fixtures/viewer/sequences_files/sequences.csv'
+    result = _get_sqc_identity_from_csv(path)
+    assert isinstance(result, dict)
+
+
+# Test for correctness of the string
+# Test for checking int value is 0 or 1
