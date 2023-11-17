fdeb63fd3764e90a8f7f1cd3c1456879c4732df7
Virginie Grosboillot <vestalisvirginis@gmail.com>
2023-11-17 10:46:57 +0000

    test case load op

--- test/unit/all_jobs/transform_job/test_load.py
+++ test/unit/all_jobs/transform_job/test_load.py
@@ -9,3 +9,10 @@ def test_load():
     context = build_op_context()
     result = load(context, PipeConfig(source='a'))
     assert isinstance(result, types.GeneratorType)
+
+
+def test_load_files():
+    context = build_op_context()
+    result = load(context, PipeConfig(source='test/fixtures/assets_testing_folder/transform/genbank'))
+    assert isinstance(result, types.GeneratorType)
+    assert len([r for r in result]) == 6
