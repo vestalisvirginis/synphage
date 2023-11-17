c2fc9f1d28177cfdb172ca437a3ba350042590de
Virginie Grosboillot <54766741+vestalisvirginis@users.noreply.github.com>
2023-11-17 11:48:19 +0100

    Feature test job transform 2 (#29)
    * fix test setup_config
    
    * test case load op
    
    * test cases parse blastn + data

--- test/unit/all_jobs/transform_job/test_setup_config.py
+++ test/unit/all_jobs/transform_job/test_setup_config.py
@@ -2,4 +2,5 @@ from synphage.jobs import setup_config, PipeConfig
 
 def test_setup_config():
     result = setup_config(PipeConfig(source='a'))
-    assert result == {"source": "a", "target": None, "table_dir": None, "file": "out.parquet"}
+    assert isinstance(result, PipeConfig)
+    assert result == PipeConfig(source='a', target=None, table_dir=None, file='out.parquet')
