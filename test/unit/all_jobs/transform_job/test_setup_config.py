424f529237f4840ece1a4ce0185234116c5720ca
Virginie Grosboillot <vestalisvirginis@gmail.com>
2023-11-17 11:24:15 +0000

    fix setup_config_test

--- test/unit/all_jobs/transform_job/test_setup_config.py
+++ test/unit/all_jobs/transform_job/test_setup_config.py
@@ -2,8 +2,8 @@ from synphage.jobs import setup_config, PipeConfig
 
 
 def test_setup_config():
-    result = setup_config(PipeConfig(source="a"))
+    result = setup_config(PipeConfig(source="a", target='b', table_dir='c', file='d.parquet'))
     assert isinstance(result, PipeConfig)
     assert result == PipeConfig(
-        source="a", target=None, table_dir=None, file="out.parquet"
+        source="a", target='b', table_dir='c', file='d.parquet'
     )
