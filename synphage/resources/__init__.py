3025827a81ee8cf5368d91cd7ea89391f7c18840
Virginie Grosboillot <vestalisvirginis@gmail.com>
2023-12-04 15:30:08 +0000

    fix coverage

--- synphage/resources/__init__.py
+++ synphage/resources/__init__.py
@@ -7,8 +7,8 @@ from dagster import EnvVar, FilesystemIOManager
 
 
 RESOURCES_LOCAL = {
-    "ncbi_connection": ncbi_resource,
-    "parquet_io_manager": LocalParquetIOManager(base_dir=EnvVar("PHAGY_DIRECTORY")),
+    #"ncbi_connection": ncbi_resource,
+    #"parquet_io_manager": LocalParquetIOManager(base_dir=EnvVar("PHAGY_DIRECTORY")),
     "io_manager": FilesystemIOManager(
         base_dir="/".join([os.getenv("PHAGY_DIRECTORY"), "fs"])
     ),
--- synphage/resources/parquet_io_manager.py
+++ synphage/resources/parquet_io_manager.py
@@ -6,22 +6,22 @@ from pyspark.sql import SparkSession
 class LocalParquetIOManager(ConfigurableIOManager):
     base_dir: str
 
-    def _get_path(self, context):
-        return "/".join(
-            [
-                context.resource_config["base_dir"],
-                context.metadata["tables"],
-                context.metadata["name"],
-            ]
-        )
-        # return "/".join([context.resource_config["base_dir"], context.op_config["output_folder"], context.op_config["name"]]) AttributeError: 'OutputContext' object has no attribute 'op_config'
+    # def _get_path(self, context):
+    #     return "/".join(
+    #         [
+    #             context.resource_config["base_dir"],
+    #             context.metadata["tables"],
+    #             context.metadata["name"],
+    #         ]
+    #     )
+    #     # return "/".join([context.resource_config["base_dir"], context.op_config["output_folder"], context.op_config["name"]]) AttributeError: 'OutputContext' object has no attribute 'op_config'
 
-    def handle_output(self, context, obj):
-        if context.metadata["parquet_managment"] == "overwrite":
-            obj.coalesce(1).write.mode("overwrite").parquet(self._get_path(context))
-        else:
-            obj.coalesce(1).write.mode("append").parquet(self._get_path(context))
+    # def handle_output(self, context, obj):
+    #     if context.metadata["parquet_managment"] == "overwrite":
+    #         obj.coalesce(1).write.mode("overwrite").parquet(self._get_path(context))
+    #     else:
+    #         obj.coalesce(1).write.mode("append").parquet(self._get_path(context))
 
-    def load_input(self, context):  # context: InputContext
-        spark = SparkSession.builder.getOrCreate()
-        return spark.read.parquet(self._get_path(context.upstream_output))
+    # def load_input(self, context):  # context: InputContext
+    #     spark = SparkSession.builder.getOrCreate()
+    #     return spark.read.parquet(self._get_path(context.upstream_output))
