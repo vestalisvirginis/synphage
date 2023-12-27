9135ee177caccccbd514fdaaa4bad3d1ff449c78
Virginie Grosboillot <vestalisvirginis@gmail.com>
2023-12-27 14:57:50 +0000

    uncomment

--- synphage/assets/ncbi_connect/accession.py
+++ synphage/assets/ncbi_connect/accession.py
@@ -40,6 +40,7 @@ def accession_count(context) -> int:
         term=context.op_config["keyword"]
     )
     _result = context.resources.ncbi_connection.conn.read(_query)
+    _query.close()
     _nucleotide = _get_ncbi_count_result(_result, context.op_config["database"])
     _num_rows = int(_nucleotide.count)
     context.log_event(
@@ -48,89 +49,91 @@ def accession_count(context) -> int:
     return _num_rows
 
 
-# ncbi_query_config_search = {
-#     "use_history": Field(
-#         str, description="Yes/No value for history", default_value="y"
-#     ),
-#     "idtype": Field(str, description="Options for acc", default_value="acc"),
-# }
+ncbi_query_config_search = {
+    "use_history": Field(
+        str, description="Yes/No value for history", default_value="y"
+    ),
+    "idtype": Field(str, description="Options for acc", default_value="acc"),
+}
 
 
-# @asset(
-#     required_resource_keys={"ncbi_connection"},
-#     config_schema={**ncbi_query_config, **ncbi_query_config_search},
-#     description="Getting all accession Ids corresponding to keyword(s)",
-#     compute_kind="NCBI",
-#     metadata={"owner": "Virginie Grosboillot"},
-# )
-# def accession_ids(context, accession_count):
-#     _search = context.resources.ncbi_connection.conn.esearch(
-#         db=context.op_config["database"],
-#         term=context.op_config["keyword"],
-#         retmax=accession_count,
-#         usehistory=context.op_config["use_history"],
-#         idtype=context.op_config["idtype"],
-#     )
-#     _result = context.resources.ncbi_connection.conn.read(_search)
-#     context.log_event(
-#         AssetObservation(
-#             asset_key="accession_ids", metadata={"num_rows": len(_result["IdList"])}
-#         )
-#     )
-#     _search.close()
-#     return _result
+@asset(
+    required_resource_keys={"ncbi_connection"},
+    config_schema={**ncbi_query_config, **ncbi_query_config_search},
+    description="Getting all accession Ids corresponding to keyword(s)",
+    compute_kind="NCBI",
+    metadata={"owner": "Virginie Grosboillot"},
+)
+def accession_ids(context, accession_count):
+    context.log.info('Starting search')
+    _search = context.resources.ncbi_connection.conn.esearch(
+        db=context.op_config["database"],
+        term=context.op_config["keyword"],
+        retmax=accession_count,
+        usehistory=context.op_config["use_history"],
+        idtype=context.op_config["idtype"],
+    )
+    context.log.info('The searched is finished')
+    _result = context.resources.ncbi_connection.conn.read(_search)
+    context.log_event(
+        AssetObservation(
+            asset_key="accession_ids", metadata={"num_rows": len(_result["IdList"])}
+        )
+    )
+    _search.close()
+    return _result
 
 
-# download_folder_config = {
-#     "output_directory": Field(
-#         str,
-#         description="Path to folder",
-#         default_value="download",
-#     )
-# }
+download_folder_config = {
+    "output_directory": Field(
+        str,
+        description="Path to folder",
+        default_value="download",
+    )
+}
 
 
-# @asset(config_schema=download_folder_config, compute_kind="python")
-# def downloaded_genomes(context) -> List[str]:
-#     _download_path = "/".join(
-#         [os.getenv(EnvVar("PHAGY_DIRECTORY")), context.op_config["output_directory"]]
-#     )
-#     return list(map(lambda x: Path(x).stem, os.listdir(_download_path)))
+@asset(config_schema=download_folder_config, compute_kind="python")
+def downloaded_genomes(context) -> List[str]:
+    _download_path = "/".join(
+        [os.getenv(EnvVar("PHAGY_DIRECTORY")), context.op_config["output_directory"]]
+    )
+    return list(map(lambda x: Path(x).stem, os.listdir(_download_path)))
 
 
-# ncbi_query_config_fetch = {
-#     "database": Field(str, description="Database identifier", default_value="nuccore"),
-#     "rettype": Field(str, description="File format", default_value="gb"),  # gbwithparts
-#     # "batch": Field(int, description="Options for acc", default_value=10)
-# }
+ncbi_query_config_fetch = {
+    "database": Field(str, description="Database identifier", default_value="nuccore"),
+    "rettype": Field(str, description="File format", default_value="gb"),  # gbwithparts
+    # "batch": Field(int, description="Options for acc", default_value=10)
+}
 
 
-# @asset(
-#     required_resource_keys={"ncbi_connection"},
-#     config_schema={**download_folder_config, **ncbi_query_config_fetch},
-#     description="Download records one by one from the ncbi database",
-#     compute_kind="NCBI",
-#     metadata={"owner": "Virginie Grosboillot"},
-# )
-# def fetch_genome(context, accession_ids, downloaded_genomes) -> List[str]:
-#     _A = set(accession_ids["IdList"])
-#     _B = set(downloaded_genomes)
-#     _C = _A.difference(_B)
-#     context.log.info(f"Number of NOT Downloaded: {len(_C)}")
-#     _path = context.op_config["output_directory"]
+@asset(
+    required_resource_keys={"ncbi_connection"},
+    config_schema={**download_folder_config, **ncbi_query_config_fetch},
+    description="Download records one by one from the ncbi database",
+    compute_kind="NCBI",
+    metadata={"owner": "Virginie Grosboillot"},
+)
+def fetch_genome(context, accession_ids, downloaded_genomes) -> List[str]:
+    _A = set(accession_ids["IdList"])
+    _B = set(downloaded_genomes)
+    _C = _A.difference(_B)
+    context.log.info(f"Number of NOT Downloaded: {len(_C)}")
+    _path = context.op_config["output_directory"]
 
-#     for _entry in list(_C):
-#         _r = context.resources.ncbi_connection.conn.efetch(
-#             db=context.op_config["database"],
-#             id=_entry,
-#             rettype=context.op_config["rettype"],
-#             retmax=1,
-#             webenv=accession_ids["WebEnv"],
-#             query_key=accession_ids["QueryKey"],
-#         )
+    for _entry in list(_C):
+        _r = context.resources.ncbi_connection.conn.efetch(
+            db=context.op_config["database"],
+            id=_entry,
+            rettype=context.op_config["rettype"],
+            retmax=1,
+            webenv=accession_ids["WebEnv"],
+            query_key=accession_ids["QueryKey"],
+        )
 
-#         _file_name = f"{_path}/{_entry}.gb"
-#         with open(_file_name, "w") as _writer:
-#             _writer.write(_r.read())
+        _file_name = f"{_path}/{_entry}.gb"
+        with open(_file_name, "w") as _writer:
+            _writer.write(_r.read())
 
-#     return list(map(lambda x: f"{_path}/{x}.gb", accession_ids["IdList"]))
+    return list(map(lambda x: f"{_path}/{x}.gb", accession_ids["IdList"]))
