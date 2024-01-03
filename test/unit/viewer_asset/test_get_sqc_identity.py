7617542d42beb18922e92f23c80947c84730789f
Virginie Grosboillot <54766741+vestalisvirginis@users.noreply.github.com>
2024-01-03 17:38:48 +0100

    Feature viewer test (#42)
    * test cases viewer
    
    * include legend file

--- test/unit/viewer_asset/test_get_sqc_identity.py
+++ test/unit/viewer_asset/test_get_sqc_identity.py
@@ -1,11 +1,11 @@
-# from synphage.assets.viewer.viewer import _get_sqc_identity_from_csv
+from synphage.assets.viewer.viewer import _get_sqc_identity_from_csv
 
 
-# def test_get_sqc_identity_from_csv():
-#     path = "test/fixtures/viewer/sequences_files/sequences.csv"
-#     result = _get_sqc_identity_from_csv(path)
-#     assert isinstance(result, dict)
-
+def test_get_sqc_identity_from_csv():
+    path = "test/fixtures/viewer/sequences_files/sequences.csv"
+    result = _get_sqc_identity_from_csv(path)
+    assert isinstance(result, dict)
+    assert {'TT_000001.gb': 0, 'TT_000002.gb': 1}
 
 # # Test for correctness of the string
 # # Test for checking int value is 0 or 1
