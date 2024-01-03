6ea78e8d7eb07330f2bf90866d0c5536ce42f99a
Virginie Grosboillot <vestalisvirginis@gmail.com>
2024-01-03 15:25:09 +0000

    fix keyword variable

--- test/unit/accession_asset/test_accession_count.py
+++ test/unit/accession_asset/test_accession_count.py
@@ -11,8 +11,7 @@ def test_accession_count(mock_env_ncbi_count):
     context = build_asset_context(resources={"ncbi_connection": NCBIConnection(email=os.getenv("EMAIL"), api_key=os.getenv("API_KEY"))})
     result = accession_count(context)
     assert isinstance(result, int)
-    assert result == 579
-    # should be 2
+    assert result == 2
 
 
 def test_accession_count_asset(mock_env_ncbi_count):
@@ -20,4 +19,4 @@ def test_accession_count_asset(mock_env_ncbi_count):
     result = materialize_to_memory(assets, resources={"ncbi_connection": NCBIConnection(email=os.getenv("EMAIL"), api_key=os.getenv("API_KEY"))})
     assert result.success
     acc_count = result.output_for_node("accession_count")
-    assert acc_count == 579
+    assert acc_count == 2
