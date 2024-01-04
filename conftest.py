e7ff56f825f71bb9ea642c351486fcc3da794538
Virginie Grosboillot <vestalisvirginis@gmail.com>
2024-01-04 16:50:28 +0000

    remove all use of pyspark

--- conftest.py
+++ conftest.py
@@ -1,10 +1,4 @@
-# import os
-# import warnings
 import pytest
-# from pyspark.sql import SparkSession
-#import logging
-
-#logger = logging.getLogger(__name__)
 
 
 @pytest.fixture
@@ -65,16 +59,3 @@ def mock_env_phagy_dir_transform_step3(monkeypatch):
 def mock_env_phagy_dir_synteny(monkeypatch):
     monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/synteny")
     monkeypatch.setenv("SEQUENCE_FILE", "sequences.csv")
-# @pytest.fixture(scope="session")
-# def spark():
-#     try:
-#         #logger = logging.getLogger("py4j")
-#         #ogger.setLevel(logging.ERROR)
-#         spark_session = SparkSession.builder.config(
-#             "spark.driver.memory", "2g"
-#         ).getOrCreate()
-#         yield spark_session
-#     except:
-#         pass
-#     finally:
-#         spark_session.stop()
