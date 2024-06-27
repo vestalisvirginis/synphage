f799c7d35e85056502ae5dd235693c9aa7561f1f
Virginie Grosboillot <vestalisvirginis@gmail.com>
2024-06-27 12:55:08 +0000

    fix deprecated egquery function and simplify assets

--- synphage/assets/ncbi_connect/accession.py
+++ synphage/assets/ncbi_connect/accession.py
@@ -2,29 +2,15 @@ from dagster import asset, AssetObservation, EnvVar, Config, ConfigArgumentWarni
 from pydantic import Field
 
 import os
-import tempfile
+import warnings
 
-from toolz import first
-from collections import namedtuple
 from typing import List
 from pathlib import Path
-from datetime import datetime
-import warnings
+
+from synphage.synphage_settings import DOWNLOAD_DIR
 
 
 warnings.filterwarnings("ignore", category=ConfigArgumentWarning)
-
-
-TEMP_DIR = tempfile.gettempdir()
-
-NucleotideRecord = namedtuple("NucleotideRecord", "dbname,menu,count,status")
-
-
-def _get_ncbi_count_result(result, dbname) -> NucleotideRecord:
-    _origin = result["eGQueryResult"]
-    return NucleotideRecord(
-        *first(filter(lambda x: x["DbName"] == dbname, _origin)).values()
-    )
 
 
 class QueryConfig(Config):
@@ -35,7 +21,6 @@ class QueryConfig(Config):
     use_history: str = Field(default="y", description="Yes/No value for history")
     idtype: str = Field(default="acc", description="Options for acc")
     rettype: str = Field(default="gb", description="File format")  # gbwithparts
-    download_dir: str = Field(default="download", description="Path to download folder")
 
 
 @asset(
@@ -44,13 +29,12 @@ class QueryConfig(Config):
     metadata={"owner": "Virginie Grosboillot"},
 )
 def setup_query_config(config: QueryConfig) -> QueryConfig:
-    """Source/target dirs and    file output"""
+    """Search parameters"""
     return config
 
 
 @asset(
     required_resource_keys={"ncbi_connection"},
-    # config_schema=ncbi_query_config,
     description="Getting the number of records matching the keyword(s) in the specified database",
     compute_kind="NCBI",
     io_manager_key="io_manager",
@@ -58,27 +42,26 @@ def setup_query_config(config: QueryConfig) -> QueryConfig:
 )
 def accession_count(context, setup_query_config: QueryConfig) -> int:
     # Search key - default: Myoalterovirus (2 entries in NCBI database Jan 2024)
-    # keyword = context.op_config["search_key"]
     keyword = setup_query_config.search_key
+    db = setup_query_config.database
     context.log.info(f"Search key(s): {keyword}")
     # Query
-    _query = context.resources.ncbi_connection.conn.egquery(term=keyword)
+    _query = context.resources.ncbi_connection.conn.esearch(term=keyword, db=db)
     _result = context.resources.ncbi_connection.conn.read(_query)
     _query.close()
     # Extract number of record for keyword
-    # _nucleotide = _get_ncbi_count_result(_result, context.op_config["database"])
-    _nucleotide = _get_ncbi_count_result(_result, setup_query_config.database)
-    _num_rows = int(_nucleotide.count)
+    _num_rows = int(_result['Count'])
+    _ncbi_query = _result['QueryTranslation']
     context.log_event(
         AssetObservation(asset_key="accession_count", metadata={"num_rows": _num_rows})
     )
 
     # Asset user metadata
-    _time = datetime.now()
     context.add_output_metadata(
         metadata={
-            "text_metadata": f"End of query: {_time.isoformat()} (UTC).",
             "search_word(s)": keyword,
+            "database": db,
+            "NCBI_query": _ncbi_query,
             "num_hits": _num_rows,
         }
     )
@@ -88,7 +71,6 @@ def accession_count(context, setup_query_config: QueryConfig) -> int:
 
 @asset(
     required_resource_keys={"ncbi_connection"},
-    # config_schema=ncbi_query_config,
     description="Getting all accession Ids corresponding to keyword(s)",
     compute_kind="NCBI",
     io_manager_key="io_manager",
@@ -96,18 +78,15 @@ def accession_count(context, setup_query_config: QueryConfig) -> int:
 )
 def accession_ids(context, accession_count, setup_query_config: QueryConfig) -> dict:
     # Search key - default: Myoalterovirus (2 entries in NCBI database Jan 2024)
-    # keyword = context.op_config["search_key"]
     keyword = setup_query_config.search_key
+    db = setup_query_config.database
     context.log.info(f"Search key(s): {keyword}")
     # Search
     context.log.info("Start NCBI database search")
     _search = context.resources.ncbi_connection.conn.esearch(
-        # db=context.op_config["database"],
-        db=setup_query_config.database,
+        db=db,
         term=keyword,
         retmax=accession_count,
-        # usehistory=context.op_config["use_history"],
-        # idtype=context.op_config["idtype"],
         usehistory=setup_query_config.use_history,
         idtype=setup_query_config.idtype,
     )
@@ -121,11 +100,10 @@ def accession_ids(context, accession_count, setup_query_config: QueryConfig) -> 
     _search.close()
 
     # Asset user metadata
-    _time = datetime.now()
     context.add_output_metadata(
         metadata={
-            "text_metadata": f"End of search: {_time.isoformat()} (UTC).",
             "search_word(s)": keyword,
+            "database": db,
             "num_retrived_ids": len(_result["IdList"]),
             "id_preview": _result["IdList"],
         }
@@ -134,38 +112,23 @@ def accession_ids(context, accession_count, setup_query_config: QueryConfig) -> 
     return _result
 
 
-# download_folder_config = {
-#     "output_directory": Field(
-#         str,
-#         description="Path to folder",
-#         default_value="download",
-#     )
-# }
-
-
 @asset(
-    # config_schema=download_folder_config,
-    description="In case of multiple search, checked what sequence have already been downloeded",
+    description="In case of multiple searches, check what sequences have already been downloaded.",
     compute_kind="python",
     io_manager_key="io_manager",
     metadata={"owner": "Virginie Grosboillot"},
 )
-def downloaded_genomes(context, setup_query_config: QueryConfig) -> List[str]:
+def downloaded_genomes(context) -> List[str]:
     # Download directory
-    _download_path = str(
-        Path(os.getenv(EnvVar("DATA_DIR"), TEMP_DIR))
-        # / context.op_config["output_directory"]
-        / setup_query_config.download_dir
-    )
+    _download_path = DOWNLOAD_DIR
     os.makedirs(_download_path, exist_ok=True)
     # List file in download directory
     _downloaded_files = list(map(lambda x: Path(x).stem, os.listdir(_download_path)))
 
     # Asset user metadata
-    _time = datetime.now()
     context.add_output_metadata(
         metadata={
-            "text_metadata": f"Downloaded files last update: {_time.isoformat()} (UTC).",
+            "folder": _download_path,
             "num_files": len(_downloaded_files),
             "preview": _downloaded_files,
         }
@@ -176,7 +139,6 @@ def downloaded_genomes(context, setup_query_config: QueryConfig) -> List[str]:
 
 @asset(
     required_resource_keys={"ncbi_connection"},
-    # config_schema={**download_folder_config, **ncbi_query_config},
     description="Download records one by one from the ncbi database",
     compute_kind="NCBI",
     io_manager_key="io_manager",
@@ -191,19 +153,17 @@ def fetch_genome(
     _C = _A.difference(_B)
     context.log.info(f"Number of files NOT downloaded: {len(_C)}")
     # Path to download
-    _download_path = str(
-        Path(os.getenv(EnvVar("DATA_DIR"), TEMP_DIR))
-        # / context.op_config["output_directory"]
-        / setup_query_config.download_dir
-    )
-    context.log.info(f"Path to download: {_download_path}")
+    _download_path = DOWNLOAD_DIR
+    context.log.info(f"Download in process to: {_download_path}")
     # Fetch and write files
+    _new_download = []
+    count = 0
     for _entry in list(_C):
+        count +=1
+        context.log.info(f"Downloading {_entry}, ({count/len(_C)})")
         _r = context.resources.ncbi_connection.conn.efetch(
-            # db=context.op_config["database"],
             db=setup_query_config.database,
             id=_entry,
-            # rettype=context.op_config["rettype"],
             rettype=setup_query_config.rettype,
             retmax=1,
             webenv=accession_ids["WebEnv"],
@@ -211,19 +171,22 @@ def fetch_genome(
         )
 
         _file_name = str(Path(_download_path) / f"{_entry}.gb")
+        _new_download.append(_file_name)
         with open(_file_name, "w") as _writer:
             _writer.write(_r.read())
+        context.log.info(f"Done")
 
+    context.log.info(f"Download completed successfully!")
     _all_ids = _B.union(_A)
     _genomes = list(map(lambda x: str(Path(_download_path) / f"{x}.gb"), _all_ids))
 
     # Asset user metadata
-    _time = datetime.now()
     context.add_output_metadata(
         metadata={
-            "text_metadata": f"Dowloaded files latest status: {_time.isoformat()} (UTC).",
-            "num_files": len(_genomes),
-            "preview": _genomes,
+            "downloaded_files": len(_new_download),
+            "dowloaded_files_preview": _new_download,
+            "total_files": len(_genomes),
+            "total_preview": _genomes,
         }
     )
 
---
+++ synphage/assets/ncbi_connect/downloaded_file_transfer.py
@@ -0,0 +1,63 @@
+from dagster import asset
+
+import os
+import shutil
+import pickle
+
+from pathlib import Path
+from typing import List
+from collections import namedtuple
+
+from synphage.synphage_settings import FILESYSTEM_DIR, GENBANK_DIR
+
+DownloadRecord = namedtuple("DownloadRecord", "new,history")
+
+@asset(
+description="Transfer new downloaded files to the genbank folder and harmonise naming of the files",
+compute_kind="Python",
+metadata={"owner": "Virginie Grosboillot"},
+)
+def download_to_genbank(context, fetch_genome) -> DownloadRecord:
+    # Check if history of transferred files
+    _path_history = (
+        Path(FILESYSTEM_DIR)
+        / "download_to_genbank"
+    )
+
+    if os.path.exists(_path_history):
+        _history_files = pickle.load(open(_path_history, "rb")).history
+        context.log.info("Transferred file history loaded")
+    else:
+        _history_files = []
+        context.log.info("No transfer history")
+
+    # Transfer only new files
+    _T = list(set(fetch_genome).difference(set(_history_files)))
+    context.log.info(f"Number of genomes to transfer: {len(_T)}")
+
+    # Path to genbank folder
+    _gb_path = GENBANK_DIR
+    os.makedirs(_gb_path, exist_ok=True)
+
+    # Harmonise file name
+    _new_transfer = []
+    for _file in _T:
+        _output_file = str(Path(_gb_path) / f"{Path(_file).stem.replace('.', '_')}.gb")
+        shutil.copy2(
+            _file,
+            _output_file,
+        )
+        _new_transfer.append(Path(_output_file).name)
+        _history_files.append(_file)
+
+    context.add_output_metadata(
+        metadata={
+            "path": _gb_path,
+            "num_new_files": len(_new_transfer),
+            "new_files_preview": _new_transfer,
+            "total_files": len(_history_files),
+            "total_files_preview": _history_files,
+        },
+    )
+
+    return DownloadRecord(_new_transfer, _history_files)
