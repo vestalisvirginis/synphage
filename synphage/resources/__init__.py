099080b9d629323fe9febfe0d2fdfb58d576a7d4
Virginie Grosboillot <vestalisvirginis@gmail.com>
2024-06-27 12:54:25 +0000

    change io manager resource to new path config

--- synphage/resources/__init__.py
+++ synphage/resources/__init__.py
@@ -1,16 +1,12 @@
-import os
-import tempfile
-
-from pathlib import Path
-
 from .ncbi_resource import ncbi_resource
+from synphage.synphage_settings import FILESYSTEM_DIR
 
-from dagster import EnvVar, FilesystemIOManager
+from dagster import FilesystemIOManager
 
 
 RESOURCES_LOCAL = {
     "ncbi_connection": ncbi_resource,
     "io_manager": FilesystemIOManager(
-        base_dir=str(Path(os.getenv(EnvVar("DATA_DIR"), tempfile.gettempdir())) / "fs")
+        base_dir=FILESYSTEM_DIR
     ),
 }
