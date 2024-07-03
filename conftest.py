20ac47af0d4789bacd94d021e476bab605e217b1
Virginie Grosboillot <vestalisvirginis@gmail.com>
2024-07-03 09:18:05 +0000

    test cases local resources

--- conftest.py
+++ conftest.py
@@ -2,55 +2,66 @@ import pytest
 
 
 @pytest.fixture
+def mock_env_input_validator(monkeypatch):
+    monkeypatch.delenv('DATA_DIR')
+    monkeypatch.setenv('OUTPUT_DIR', '/output_folder')
+
+@pytest.fixture
+def mock_env_output_validator(monkeypatch):
+    monkeypatch.setenv('DATA_DIR', '/input_folder')
+    monkeypatch.delenv('OUTPUT_DIR')
+
+@pytest.fixture
 def mock_env_ncbi_connect(monkeypatch):
     monkeypatch.setenv("EMAIL", "name@domain.com")
     monkeypatch.setenv("API_KEY", "jhd6hdz778ahjeahj8889")
 
 @pytest.fixture
 def mock_env_ncbi_download_pos(monkeypatch):
-    monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/ncbi_download/positive")
+    monkeypatch.setenv("OUTPUT_DIR", "test/fixtures/assets_testing_folder/ncbi_download/positive")
 
 @pytest.fixture
 def mock_env_ncbi_download_neg(monkeypatch):
-    monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/ncbi_download/negative")
+    monkeypatch.setenv("OUTPUT_DIR", "test/fixtures/assets_testing_folder/ncbi_download/negative")
 
 @pytest.fixture
 def mock_env_ncbi_fetch(monkeypatch):
-    monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/ncbi_download/fetch")
+    monkeypatch.chdir('test/fixtures/assets_testing_folder/ncbi_download/fetch')
+    monkeypatch.setenv("OUTPUT_DIR", "test/fixtures/assets_testing_folder/ncbi_download/fetch")
 
 @pytest.fixture
 def mock_env_sequence_check(monkeypatch):
-    monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/sequence_quality")
+    monkeypatch.setenv("OUTPUT_DIR", "test/fixtures/assets_testing_folder/sequence_quality")
 
 @pytest.fixture
 def mock_env_sequence_check_with_history(monkeypatch):
-    monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/sequence_quality_with_history")
+    monkeypatch.setenv("OUTPUT_DIR", "test/fixtures/assets_testing_folder/sequence_quality_with_history")
 
 @pytest.fixture
 def mock_env_phagy_dir_blasting(monkeypatch):
-    monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/blasting")
+    monkeypatch.setenv("OUTPUT_DIR", "test/fixtures/assets_testing_folder/blasting")
 
 @pytest.fixture
 def mock_env_phagy_dir_blasting_with_history(monkeypatch):
-    monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/blasting_with_history")
+    monkeypatch.setenv("OUTPUT_DIR", "test/fixtures/assets_testing_folder/blasting_with_history")
 
 @pytest.fixture
 def mock_env_phagy_dir_transform(monkeypatch):
-    monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/transform")
+    monkeypatch.setenv("OUTPUT_DIR", "test/fixtures/assets_testing_folder/transform")
 
 @pytest.fixture
 def mock_env_phagy_dir_transform_step3(monkeypatch):
-    monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/transform_3")
+    monkeypatch.setenv("OUTPUT_DIR", "test/fixtures/assets_testing_folder/transform_3")
 
 @pytest.fixture
 def mock_env_phagy_dir_synteny(monkeypatch):
-    monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/synteny")
+    monkeypatch.setenv("OUTPUT_DIR", "test/fixtures/assets_testing_folder/synteny")
 
 @pytest.fixture
 def mock_env_phagy_dir_synteny_no_csv(monkeypatch):
-    monkeypatch.setenv("DATA_DIR", "test/fixtures/assets_testing_folder/synteny_no_csv")
+    monkeypatch.setenv("OUTPUT_DIR", "test/fixtures/assets_testing_folder/synteny_no_csv")
 
 @pytest.fixture
 def mock_env_phagy_dir_none(monkeypatch):
-    monkeypatch.delenv("DATA_DIR", raising=False)
+    monkeypatch.delenv("OUTPUT_DIR", raising=False)
     
