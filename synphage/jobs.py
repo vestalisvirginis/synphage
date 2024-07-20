308c2606b818aa3119b4a280010059a8b25f1916
Virginie Grosboillot <vestalisvirginis@gmail.com>
2024-07-20 15:59:56 +0000

    rename job assets

--- synphage/jobs.py
+++ synphage/jobs.py
@@ -15,61 +15,61 @@ warnings.filterwarnings("ignore", category=ConfigArgumentWarning)
 
 # Job 1 : get data from the user
 get_user_data = define_asset_job(
-    name="get_user_data",
+    name="step_1a_get_user_data",
     selection=(
-        AssetSelection.groups("Users_input")
-        | AssetSelection.groups("Status")
+        AssetSelection.groups("users_input")
+        | AssetSelection.groups("status")
         & AssetSelection.assets("genbank_history").downstream(depth=2)
     ),
 )
 
 # Job 2 : download gb files from the ncbi database
 download = define_asset_job(
-    name="download",
+    name="step_1b_download",
     selection=(
-        AssetSelection.groups("NCBI_connect")
-        | AssetSelection.groups("Status")
+        AssetSelection.groups("ncbi_connect")
+        | AssetSelection.groups("status")
         & AssetSelection.assets("genbank_history").downstream(depth=2)
     ),
 )
 
 # Job 3 : validations with to reload + refresh UI
 validations = define_asset_job(
-    name="make_validation",
-    selection=AssetSelection.groups("Status")
+    name="step_2_make_validation",
+    selection=AssetSelection.groups("status")
     & AssetSelection.assets("reload_ui_asset").downstream(depth=3),
 )
 
 # Job 4 : blastn
 blastn = define_asset_job(
-    name="make_blastn",
+    name="step_3a_make_blastn",
     selection=(
         AssetSelection.assets("append_processed_df")
-        | AssetSelection.groups("Blaster")
+        | AssetSelection.groups("blaster")
         & AssetSelection.assets("create_fasta_n").downstream()
     ),
 )
 
 # Job 5 : blastp
 blastp = define_asset_job(
-    name="make_blastp",
+    name="step_3b_make_blastp",
     selection=(
         AssetSelection.assets("append_processed_df")
-        | AssetSelection.groups("Blaster")
+        | AssetSelection.groups("blaster")
         & AssetSelection.assets("create_fasta_p").downstream()
     ),
 )
 
 # Job 6 : blastn and blastp combined
 all_blast = define_asset_job(
-    name="make_all_blast",
+    name="step_3c_make_all_blast",
     selection=(
-        AssetSelection.assets("append_processed_df") | AssetSelection.groups("Blaster")
+        AssetSelection.assets("append_processed_df") | AssetSelection.groups("blaster")
     ),
 )
 
 # Job 7 : create the synteny diagram
 plot = define_asset_job(
-    name="make_plot",
-    selection=AssetSelection.groups("Viewer"),
+    name="step_4_make_plot",
+    selection=AssetSelection.groups("viewer"),
 )
