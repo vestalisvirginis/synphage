5503126c839ab52be9b34b302e0648a7baed4376
Virginie Grosboillot <vestalisvirginis@gmail.com>
2024-07-22 09:50:21 +0000

    include mor columns in the final dataframe

--- synphage/assets/blaster/sql/gene_presence.sql
+++ synphage/assets/blaster/sql/gene_presence.sql
@@ -1,10 +1,6 @@
-with blastn as (select COLUMNS('^(query|source)_.*$'), percentage_of_identity from read_parquet('{}')),
-     locus as (select *, key as query_key, name as query_name, id as query_id, locus_tag as query_locus_tag, start_sequence as query_start_sequence, end_sequence as query_end_sequence, strand as query_strand_sequence from read_parquet('{}')),
-     result as(select key as source_key, name as source_name, id as source_id, locus_tag as source_locus_tag, start_sequence as source_start_sequence, end_sequence as source_end_sequence, strand as source_strand_sequence from locus),
-     joinedlocus as (select *, from locus B left join blastn A using('query_key'))
-select 
-    C.*, D.*
-FROM 
-    joinedlocus D
-LEFT JOIN
-    result C using('source_key')
+with blastn as (select * exclude(query_id, query_strand) from (select COLUMNS('^(query|source)_.*$'), percentage_of_identity from read_parquet('{}'))),
+      locus as (select * from read_parquet('{}')),
+      query_side as (select COLUMNS('(\w*)') AS 'query_\1' from locus),
+      source_side as (select COLUMNS('(\w*)') AS 'source_\1' from locus),
+      joinedlocus as (select B.*, A.* exclude(query_key) from query_side B left join blastn A using (query_key))
+select C.*, D.* exclude(source_key) FROM joinedlocus C LEFT JOIN source_side D using(source_key)
